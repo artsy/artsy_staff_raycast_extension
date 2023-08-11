be68db954fcf5279371d01f3232179e1c53aa8b9
Ole <ole.richter91@gmail.com>
2023-08-11 14:46:51 +0200

    Default projects

--- src/ProjectList/defaultProjects.ts
+++ src/ProjectList/defaultProjects.ts
@@ -10,21 +10,15 @@ export interface Project {
   links: ProjectLink[];
 }
 
-export const defaultProjects: Project[] = [
-  {
-    name: "Eigen",
-    links: [
-      { title: "GitHub", target: "https://github.com/artsy/eigen", primaryAction: true },
-      { title: "CircleCI", target: "https://app.circleci.com/pipelines/github/artsy/eigen", secondaryAction: true },
-    ],
-  },
+export const projects: Project[] = [
+  { name: "Cohesion", links: [] },
+  { name: "Diffusion", links: [] },
+  { name: "Eigen", links: [] },
   {
     name: "Force",
     links: [
       { title: "Production", target: "https://www.artsy.net" },
       { title: "Staging", target: "https://staging.artsy.net" },
-      { title: "GitHub", target: "https://github.com/artsy/force", primaryAction: true },
-      { title: "CircleCI", target: "https://app.circleci.com/pipelines/github/artsy/force", secondaryAction: true },
     ],
   },
   {
@@ -34,8 +28,8 @@ export const defaultProjects: Project[] = [
       { title: "Staging", target: "https://stagingapi.artsy.net" },
       { title: "Production Sidekiq", target: "https://api.artsy.net/admin/sidekiq" },
       { title: "Staging Sidekiq", target: "https://stagingapi.artsy.net/admin/sidekiq" },
-      { title: "GitHub", target: "https://github.com/artsy/gravity", primaryAction: true },
-      { title: "CircleCI", target: "https://app.circleci.com/pipelines/github/artsy/gravity", secondaryAction: true },
+      { title: "Sentry Production", target: "https://artsynet.sentry.io/issues/?project=157936" },
+      { title: "Sentry Staging", target: "https://artsynet.sentry.io/issues/?project=157935" },
     ],
   },
   {
@@ -43,25 +37,35 @@ export const defaultProjects: Project[] = [
     links: [
       { title: "Production", target: "https://metaphysics-production.artsy.net" },
       { title: "Staging", target: "https://metaphysics-staging.artsy.net" },
-      { title: "GitHub", target: "https://github.com/artsy/metaphysics", primaryAction: true },
-      {
-        title: "CircleCI",
-        target: "https://app.circleci.com/pipelines/github/artsy/metaphysics",
-        secondaryAction: true,
-      },
     ],
   },
+  { name: "Palette", links: [] },
+  { name: "Palette-Mobile", links: [] },
   {
     name: "Volt",
     links: [
       { title: "Production", target: "https://cms.artsy.net" },
       { title: "Staging", target: "https://cms-staging.artsy.net" },
-      { title: "GitHub", target: "https://github.com/artsy/volt", primaryAction: true },
-      {
-        title: "CircleCI",
-        target: "https://app.circleci.com/pipelines/github/artsy/volt",
-        secondaryAction: true,
-      },
+      { title: "Sentry Production", target: "https://artsynet.sentry.io/issues/?project=1281370" },
+      { title: "Sentry Staging", target: "https://artsynet.sentry.io/issues/?project=1281353" },
     ],
   },
+  { name: "Vortex", links: [] },
 ];
+
+const buildProjects = (projects: Project[]): Project[] =>
+  projects.map(({ name, links }) => ({
+    name,
+    links: [
+      { title: "GitHub", target: `https://github.com/artsy/${name.toLowerCase()}`, primaryAction: true },
+      { title: "GitHub Pull Requests", target: `https://github.com/artsy/${name.toLowerCase()}/pulls` },
+      {
+        title: "CircleCI",
+        target: `https://app.circleci.com/pipelines/github/artsy/${name.toLowerCase()}`,
+        secondaryAction: true,
+      },
+      ...links,
+    ],
+  }));
+
+export const defaultProjects: Project[] = buildProjects(projects);
